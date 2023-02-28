1cfd7260c6ac615b6c301d355c9c6467c01b936f
gonzalocandia92 <114780606+gonzalocandia92@users.noreply.github.com>
2023-02-28 03:56:28 -0300

    Last one

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ $(".option").click(function () {
   $(".option").removeClass("active");
   $(this).addClass("active");
 });
+
+
+//Primero capturamos el elemento del DOM con el id que definimos en el html
+const text = document.getElementById("animate-text");
+let str = text.innerHTML;
+text.innerHTML = "";
+
+//Declaramos una varible en la que asignaremos un valor para que sea el tiempo en ms que durará la función setTimeout
+const speed = 120;
+let i = 0;
+
+//Esta función recorre todos los caracteres nuestro texto
+const typeWriter = () => {
+  if (i < str.length) {
+    text.innerHTML += str.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
+
+//Acá ejecutamos la función
+setTimeout(typeWriter, speed);
